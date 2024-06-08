359022252b7d3518406ffbcd85c57238ecacf03d
Aakash Raj <aakashraj416@gmail.com>
2024-06-08 23:05:30 +0530

    vite dotenv file added

--- src/Services/AuthService.jsx
+++ src/Services/AuthService.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:8000";
+const API = import.meta.env.VITE_API_URL;
 
 export const RegisterUser = async (data) => {
   return await axios.post(`${API}/api/users/register`, data);
