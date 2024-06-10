064f654f6266b853e06c6c8588067763b03ef256
Aakash Raj <aakashraj416@gmail.com>
2024-06-10 16:10:57 +0530

    user role updated

--- server/Model/userModel.js
+++ server/Model/userModel.js
@@ -3,6 +3,11 @@ const userSchema = new mongoose.Schema({
   userName: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
+  role: {
+    type: String,
+    enum: ["user", "seller", "admin"],
+    default: "user",
+  },
 });
 
 const user = mongoose.model("user", userSchema);
