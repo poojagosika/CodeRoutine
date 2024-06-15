6b3700ff0542a271510d156e2a86ae24350f0db8
Pooja Gosika <pooja.gosika@gmail.com>
2024-06-15 11:16:34 +0530

    store-page/redeem-page is created

---
+++ src/Pages/Store/Config.js
@@ -0,0 +1,61 @@
+import TimeTravelTicket from "../../assets/Store/TimeTravelTicket.png";
+import PremiumSubscription from "../../assets/Store/PremiumSubscription.jpg";
+import CodeRoutineTShirt from "../../assets/Store/CodeRoutineTShirt.webp";
+import CodeRoutineCap from "../../assets/Store/CodeRoutineCap.jpg";
+import CodeRoutineKit from "../../assets/Store/CodeRoutineKit.avif";
+import CodeRoutineLaptopSleeve from "../../assets/Store/CodeRoutineLaptopSleeve.jpg";
+import CodeRoutineNotebook from "../../assets/Store/CodeRoutineNotebook.jpg";
+import CodeRoutineHoodie from "../../assets/Store/CodeRoutineHoodie.jpg";
+
+const data = [
+  {
+    image: TimeTravelTicket,
+    biggerText: "Time Travel Ticket",
+    smallerText: "For Daily Coding Challenge",
+    coins: 70,
+  },
+  {
+    image: PremiumSubscription,
+    biggerText: "30-Day Premium Subscription",
+    smallerText: "Premium",
+    coins: 5000,
+  },
+  {
+    image: CodeRoutineTShirt,
+    biggerText: "CodeRoutine T-Shirt",
+    smallerText: "Redeem our high quality t-shirts",
+    coins: 6000,
+  },
+  {
+    image: CodeRoutineCap,
+    biggerText: "CodeRoutine Cap",
+    smallerText: "Comes in black or white",
+    coins: 5500,
+  },
+  {
+    image: CodeRoutineKit,
+    biggerText: "CodeRoutine Kit",
+    smallerText: "Includes t-shirt, keychain and coaster",
+    coins: 7800,
+  },
+  {
+    image: CodeRoutineLaptopSleeve,
+    biggerText: "CodeRoutine Laptop Sleeve",
+    smallerText: "CodeRoutine's exclusive laptop sleeve",
+    coins: 8000,
+  },
+  {
+    image: CodeRoutineNotebook,
+    biggerText: "CodeRoutine Notebook",
+    smallerText: "CodeRoutine's notebook with Big-O chart",
+    coins: "10,000",
+  },
+  {
+    image: CodeRoutineHoodie,
+    biggerText: "CodeRoutine Hoodie",
+    smallerText: "Redeem our high quality hoodies",
+    coins: 9000,
+  },
+];
+
+export { data };
---
+++ src/Pages/Store/Redeem.jsx
@@ -0,0 +1,217 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import Typography from "@mui/material/Typography";
+import RedeemIcon from "@mui/icons-material/Redeem";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import StarIcon from "@mui/icons-material/Star";
+import SendIcon from "@mui/icons-material/Send";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import { Avatar, Button, CssBaseline, Grid, Stack } from "@mui/material";
+import Box from "@mui/material/Box";
+import codeRoutineLogo from "../../assets/logo.png";
+import StarsIcon from "@mui/icons-material/Stars";
+import { data } from "../Store/Config.js";
+
+export default function Redeem() {
+  return (
+    <Grid
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        flexWrap: "wrap",
+      }}
+    >
+      <CssBaseline />
+      {/* //the button is automatically taking block level, position should be fixed */}
+      <Button
+        size="small"
+        color="primary"
+        variant="contained"
+        disableElevation
+        sx={{
+          backgroundColor: "rgb(74,74,74)",
+          color: "white",
+          padding: 0.5,
+        }}
+      >
+        <StarsIcon
+          sx={{
+            width: 15,
+            color: "#ffc400",
+            mr: 1,
+          }}
+        />
+        30
+      </Button>
+      <Box
+        sx={{
+          color: "white",
+          background: "rgb(74,74,74)",
+          backgroundColor:
+            "radial-gradient(circle, rgba(74,74,74,1) 0%, rgba(5,5,5,1) 100%)",
+          height: "65vh",
+          display: "flex",
+          flexDirection: "column",
+          flexWrap: "wrap",
+          alignItems: "center",
+          justifyContent: "center",
+          textAlign: "center",
+        }}
+      >
+        <Avatar
+          alt="codeRoutineLogo"
+          src={codeRoutineLogo}
+          sx={{ width: 200, height: 200 }}
+          variant="square"
+        />
+        <Typography sx={{ fontSize: 32 }}>CodeRoutine Logo</Typography>
+        <Typography sx={{ fontSize: 18, mt: 3, color: "#bdbdbd" }}>
+          Shop in our store or redeem our products for free by using
+          CodeRoutine.
+        </Typography>
+        {/* //the Buttons are not in order in small screen, needs to be fixed */}
+        <Stack
+          direction="row"
+          spacing={3}
+          color="grey"
+          mt={3}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            textAlign: "center",
+          }}
+        >
+          <Button
+            startIcon={<RedeemIcon />}
+            sx={{
+              fontSize: 16,
+              mt: 3,
+              color: "black",
+              border: "1px dotted white",
+              backgroundColor: "white",
+            }}
+          >
+            Redeem
+          </Button>
+          <Button
+            startIcon={<AddCircleOutlineIcon />}
+            sx={{
+              fontSize: 16,
+              mt: 3,
+              color: "white",
+              border: "1px dotted white",
+              "&:hover": {
+                borderColor: "white",
+                color: "black",
+                backgroundColor: "white",
+              },
+            }}
+          >
+            Earn Coin
+          </Button>
+          <Button
+            startIcon={<StarIcon />}
+            sx={{
+              fontSize: 16,
+              mt: 3,
+              color: "white",
+              border: "1px dotted white",
+              "&:hover": {
+                borderColor: "white",
+                color: "black",
+                backgroundColor: "white",
+              },
+            }}
+          >
+            Premium
+          </Button>
+          <Button
+            startIcon={<SendIcon />}
+            sx={{
+              fontSize: 16,
+              mt: 3,
+              color: "white",
+              border: "1px dotted white",
+              "&:hover": {
+                borderColor: "white",
+                color: "black",
+                backgroundColor: "white",
+              },
+            }}
+          >
+            View Orders
+          </Button>
+        </Stack>
+      </Box>
+      <Box
+        sx={{
+          flexGrow: 5,
+          p: 8,
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "center",
+        }}
+      >
+        {data.map((storeData) => (
+          <Card sx={{ m: 5, boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px" }}>
+            <CardMedia
+              component="img"
+              height="300px"
+              width="300px"
+              image={storeData.image}
+              sx={{ width: 300, height: 300 }}
+              alt={storeData.biggerText}
+            />
+            <CardContent
+              style={{ display: "flex", justifyContent: "space-between" }}
+            >
+              <Box>
+                <Typography gutterBottom sx={{ fontSize: 12 }} component="div">
+                  {storeData.biggerText}
+                </Typography>
+                <Typography color="text.secondary" sx={{ fontSize: 10 }}>
+                  {storeData.smallerText}
+                </Typography>
+              </Box>
+              <Button
+                size="small"
+                color="primary"
+                variant="contained"
+                disableElevation
+                sx={{
+                  backgroundColor: "rgb(74,74,74)",
+                  color: "white",
+                  padding: 0.5,
+                }}
+              >
+                <StarsIcon
+                  sx={{
+                    width: 15,
+                    color: "#ffc400",
+                    mr: 1,
+                  }}
+                />
+                {storeData.coins}
+              </Button>
+            </CardContent>
+          </Card>
+        ))}
+      </Box>
+      {/* the box is taking block level, needs to be fixed */}
+      <Box
+        sx={{
+          border: "2px solid black",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          textAlign: "center",
+        }}
+      >
+        <Typography>Redeem With Code</Typography>
+        <Button>Redeem</Button>
+      </Box>
+    </Grid>
+  );
+}
