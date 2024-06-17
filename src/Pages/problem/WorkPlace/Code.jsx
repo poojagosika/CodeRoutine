0c521084e8d4bfddddf17f6e597a7f271da35b01
Aakash Raj <aakashraj416@gmail.com>
2024-06-17 13:26:53 +0530

    code area

--- src/Pages/problem/WorkPlace/Code.jsx
+++ src/Pages/problem/WorkPlace/Code.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import CodeMirror from '@uiw/react-codemirror';
+import { javascript } from '@codemirror/lang-javascript';
+import { vscodeDark } from '@uiw/codemirror-theme-vscode';
 
+import CodeIcon from '@mui/icons-material/Code';
 const Code = () => {
+    const [value, setValue] = React.useState("console.log('hello world!');");
+    const onChange = React.useCallback((val, viewUpdate) => {
+        console.log('val:', val);
+        setValue(val);
+    }, []);
     return (
-        <div>Code</div>
+        <div style={{ backgroundColor: "#1e1e1e", width: '100%', overflow: 'auto' }}
+        >
+            {/* fix do code bar */}
+            <div>
+                <CodeIcon /> Code
+            </div>
+            <CodeMirror
+                value={value}
+                theme={vscodeDark}
+                style={{ fontSize: '12px' }}
+                extensions={[javascript({ jsx: true })]}
+                onChange={onChange} />
+        </div>
     )
 }
 
