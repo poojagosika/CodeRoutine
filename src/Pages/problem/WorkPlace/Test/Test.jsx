078657314ddbbad0a7e451a6f32d04e74964ab86
Pooja Gosika <pooja.gosika@gmail.com>
2024-06-19 11:40:26 +0530

    testcases-area-bugs-fixed

--- src/Pages/problem/WorkPlace/Test/Test.jsx
+++ src/Pages/problem/WorkPlace/Test/Test.jsx
@@ -5,10 +5,29 @@ import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import { getProblemById } from "../../../../Services/AuthService";
+import { useParams } from "react-router-dom";
 export default function Test() {
-  const [showCases, setShowCases] = React.useState(false);
+  const [showCases, setShowCases] = React.useState(true);
   const [activeCase, setActiveCase] = React.useState(0);
   const [showTestResult, setShowTestResult] = React.useState(false);
+  const [questions, setQuestions] = React.useState([]);
+  const { id } = useParams();
+
+  React.useEffect(() => {
+    const fetchProblem = async () => {
+      try {
+        const response = await getProblemById(id);
+        setQuestions(response.data.problem);
+      } catch (err) {
+        setError(err);
+      } finally {
+        // setIsLoading(false);
+      }
+    };
+
+    fetchProblem();
+  }, [id]);
 
   const handleTestcase = () => {
     setShowCases(true);
@@ -23,50 +42,114 @@ export default function Test() {
   const handleplus = () => {};
 
   return (
-    <Stack spacing={2} direction="column" padding={1}>
-      <Stack spacing={2} direction="row">
-        <Button variant="text" onClick={handleTestcase}>
-          <CheckCircleOutlineIcon />
-          Testcase
-        </Button>
-        <Button variant="text" onClick={handleTestResult}>
-          <NavigateNextIcon />
-          Test Result
-        </Button>
-      </Stack>
-      {showCases && (
-        <Stack spacing={2} direction="column">
-          <Stack spacing={1} direction="row">
-            <Button
-              variant="text"
-              color={activeCase === "primary" ? "primary" : "inherit"}
-            >
-              Case 1
-            </Button>
-            <Button variant="text" onClick={handleplus}>
-              +
-            </Button>
-          </Stack>
-          <Typography variant="body1" gutterBottom>
-            nums =
-          </Typography>
-          <TextField
-            id="outlined-basic"
-            variant="outlined"
-            value="[2,7,11,15]"
-          />
-          <Typography variant="body1" gutterBottom>
-            target =
-          </Typography>
-          <TextField id="outlined-basic" variant="outlined" value="9" />
+    <div
+      style={{
+        backgroundColor: "#262626",
+        width: "100%",
+        overflow: "auto",
+        color: "white",
+      }}
+    >
+      <Stack spacing={2} direction="column">
+        <Stack
+          spacing={2}
+          direction="row"
+          style={{ backgroundColor: "#333333" }}
+        >
+          <Button
+            variant="text"
+            onClick={handleTestcase}
+            style={{
+              color: "white",
+              backgroundColor: showCases ? "#3c3c3c" : "#333333",
+            }}
+          >
+            <CheckCircleOutlineIcon
+              style={{ color: "#02b128", fontSize: 16 }}
+            />
+            Testcase
+          </Button>
+          <Button
+            variant="text"
+            onClick={handleTestResult}
+            style={{
+              color: "white",
+              backgroundColor: showTestResult ? "#3c3c3c" : "#333333",
+            }}
+          >
+            <NavigateNextIcon style={{ color: "#02b128" }} />
+            Test Result
+          </Button>
         </Stack>
-      )}
-      {showTestResult && (
-        <Typography variant="body1" gutterBottom>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-          convallis libero non eros fringilla, sed mattis risus mollis.
-        </Typography>
-      )}
-    </Stack>
+        {showCases && (
+          <Stack spacing={2} direction="column" padding={2}>
+            <Stack spacing={1} direction="row">
+              <Button
+                variant="text"
+                backgroundColor={
+                  activeCase === "primary" ? "primary" : "inherit"
+                }
+                style={{
+                  backgroundColor: "#3c3c3c",
+                  color: "white",
+                  fontSize: 14,
+                }}
+              >
+                Case 1
+              </Button>
+              <Button variant="text" onClick={handleplus}>
+                +
+              </Button>
+            </Stack>
+            <Typography variant="body1" gutterBottom>
+              Input :
+            </Typography>
+            <TextField
+              id="outlined-basic"
+              value={questions.inputDescription}
+              InputProps={{
+                style: {
+                  backgroundColor: "#3c3c3c",
+                  borderRadius: 8,
+                  color: "white",
+                  outline: "none",
+                  border: "none",
+                },
+              }}
+            />
+            <Typography variant="body1" gutterBottom>
+              Output :
+            </Typography>
+            <TextField
+              id="outlined-basic"
+              value={questions.outputDescription}
+              InputProps={{
+                style: {
+                  backgroundColor: "#3c3c3c",
+                  borderRadius: 8,
+                  color: "white",
+                  outline: "none",
+                  border: "none",
+                },
+              }}
+            />
+          </Stack>
+        )}
+        {showTestResult && (
+          <Typography
+            variant="body1"
+            gutterBottom
+            style={{
+              display: "flex",
+              alignItems: "center",
+              textAlign: "center",
+              justifyContent: "center",
+            }}
+          >
+            You must run your code first
+          </Typography>
+        )}
+      </Stack>
+    </div>
   );
 }
