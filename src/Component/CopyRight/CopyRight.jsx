4030dd9daa2a74b0215b91d55928e5f6679a92a1
Pooja Gosika <pooja.gosika@gmail.com>
2024-07-05 16:41:49 +0530

    footer modified

--- src/Component/CopyRight/CopyRight.jsx
+++ src/Component/CopyRight/CopyRight.jsx
@@ -6,10 +6,13 @@ const CopyRight = () => {
     return (
         <footer style={{ backgroundColor: '#393f48', color: 'white', marginTop: 'auto', padding: '16px' }}>
             <Container maxWidth="sm">
-                <Typography variant="h6" align="center" gutterBottom>
+                <Typography variant="h6" align="center">
                     CodeRoutine
                 </Typography>
-                <Typography variant="body2" color="textSecondary" align="center" component="p">
+                <Typography variant="subtitle1" color="#e0e0e0" align="center" component="p" gutterBottom>
+                    Our mission is to help you improve yourself and land your dream job
+                </Typography>
+                <Typography variant="body2" color="#bdbdbd" align="center" component="p">
                     &copy; {new Date().getFullYear()} CodeRoutine. All rights reserved.
                 </Typography>
             </Container>
