cfd1ea0b68f3a973d9ca01499b48f8748735f433
Aakash Raj <aakashraj416@gmail.com>
2024-07-18 13:56:38 +0530

    profile user not found

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -11,6 +11,7 @@ import Training from "./Training/Training";
 import Project from "./Project/Project";
 import Education from "./Education/Education";
 import Experience from "./Experience/Experience";
+import { toast } from "react-toastify";
 
 const Profile = () => {
   const [userProfile, setUserProfile] = React.useState(null);
@@ -23,6 +24,7 @@ const Profile = () => {
         const response = await getUserByUserName(userName);
         setUserProfile(response.data.user);
       } catch (error) {
+        toast.error(error?.response?.data?.message);
         console.error("Error fetching data:", error);
       } finally {
         setLoading(false);
@@ -31,6 +33,22 @@ const Profile = () => {
 
     fetchData();
   }, [userName]);
+
+  const hasSocialLinks = (socialLinks) => {
+    if (!socialLinks) return false;
+    const { linkedin, github, x, blog, portfolio, additional } = socialLinks;
+    return (
+      linkedin || github || x || blog || portfolio || (additional && additional.length > 0)
+    );
+  };
+  const hasPersonalInformation = (profile) => {
+    if (!profile) return false;
+    const { firstName, lastName, headline, currentPosition, education, city, gender, country } = profile;
+    return (
+      firstName || lastName || headline || currentPosition || education || city || gender || country
+    );
+  };
+
 
   if (loading) {
     return (
@@ -64,13 +82,27 @@ const Profile = () => {
             }}
           ></Box>
           <Box p={4}>
-            <PersonalInformation userProfile={userProfile} />
-            <Experience userProfile={userProfile} />
-            <Education userProfile={userProfile} />
-            <Training userProfile={userProfile} />
-            <Project userProfile={userProfile} />
-            <Skills userProfile={userProfile} />
-            <SocialLinks userProfile={userProfile} />
+            {hasPersonalInformation(userProfile?.profile) && (
+              <PersonalInformation userProfile={userProfile} />
+            )}
+            {userProfile?.experience?.length > 0 && (
+              <Experience userProfile={userProfile} />
+            )}
+            {userProfile?.education?.length > 0 && (
+              <Education userProfile={userProfile} />
+            )}
+            {userProfile?.training?.length > 0 && (
+              <Training userProfile={userProfile} />
+            )}
+            {userProfile?.project?.length > 0 && (
+              <Project userProfile={userProfile} />
+            )}
+            {userProfile?.skills?.length > 0 && (
+              <Skills userProfile={userProfile} />
+            )}
+            {hasSocialLinks(userProfile?.socialLinks) && (
+              <SocialLinks userProfile={userProfile} />
+            )}
             <ProblemsSolved problemsSolved={userProfile?.problemsSolved} />
           </Box>
         </Paper>
