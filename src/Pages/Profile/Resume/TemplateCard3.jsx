d0cfc3ee2a003ec6a6068b1cbea9bb13e791ec53
Pooja Gosika <pooja.gosika@gmail.com>
2024-07-21 17:53:24 +0530

    profile-resume-bugs

--- src/Pages/Profile/Resume/TemplateCard3.jsx
+++ src/Pages/Profile/Resume/TemplateCard3.jsx
@@ -7,20 +7,14 @@ import {
   Button,
   Box,
   Divider,
+  IconButton,
 } from "@mui/material";
 import htmlToPdfmake from "html-to-pdfmake";
 import pdfMake from "pdfmake/build/pdfmake";
 import EmailIcon from "@mui/icons-material/Email";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import { Link } from "react-router-dom";
-import {
-  LinkedIn,
-  GitHub,
-  X,
-  Language,
-  Public,
-  AccountCircle,
-} from "@mui/icons-material";
+import { LinkedIn, GitHub } from "@mui/icons-material";
 
 const TemplateCard3 = ({ user }) => {
   const resumeRef = useRef();
@@ -43,62 +37,107 @@ const TemplateCard3 = ({ user }) => {
     return `${day}/${month}/${year}`;
   };
 
-  const getDomainName = (url) => {
-    const { hostname } = new URL(url);
-    return hostname.replace("www.", "");
-  };
-
   return (
     <Card>
       <CardContent>
         <Box ref={resumeRef} p={1}>
           <Box display="flex" flexDirection="column" alignItems="center">
             <Typography variant="h3" sx={{ fontWeight: 700, color: "#004275" }}>
-              {user.profile.firstName} {user.profile.lastName}
+              {user?.profile?.firstName} {user?.profile?.lastName}
             </Typography>
-            <Typography
-              variant="body2"
+            <Box
               display="flex"
               alignItems="center"
               m={1}
-              gap={0.5}
+              gap={1}
+              justifyContent="center"
             >
-              <EmailIcon fontSize="small" sx={{ color: "text.secondary" }} />
-              {user.email}
-              <GitHub fontSize="small" sx={{ color: "text.secondary" }} />
-              <a
-                href={user.socialLinks.github}
+              <Link
+                to={`mailto:${user?.email}`}
+                style={{
+                  color: "inherit",
+                  display: "flex",
+                  alignItems: "center",
+                  textDecoration: "none",
+                }}
+              >
+                <IconButton size="small" sx={{ color: "text.secondary" }}>
+                  <EmailIcon fontSize="small" />
+                </IconButton>
+                <Typography
+                  variant="body2"
+                  component="span"
+                  sx={{ color: "text.secondary" }}
+                >
+                  {user?.email}
+                </Typography>
+              </Link>
+
+              <Link
+                to={user?.socialLinks?.github}
                 target="_blank"
                 rel="noopener noreferrer"
                 style={{ textDecoration: "none", color: "inherit" }}
+                variant="body2"
               >
-                {getDomainName(user.socialLinks.github)}
-              </a>
-              <LinkedIn fontSize="small" sx={{ color: "text.secondary" }} />
-              <a
-                href={user.socialLinks.linkedin}
+                <IconButton size="small" sx={{ color: "text.secondary" }}>
+                  <GitHub fontSize="small" />
+                </IconButton>
+                <Typography
+                  variant="body2"
+                  component="span"
+                  sx={{ color: "text.secondary" }}
+                >
+                  GitHub
+                </Typography>
+              </Link>
+
+              <Link
+                to={user?.socialLinks?.linkedin}
                 target="_blank"
                 rel="noopener noreferrer"
-                style={{ textDecoration: "none", color: "inherit" }}
+                style={{ textDecoration: "none", color: "text.secondary" }}
+                variant="body2"
               >
-                {getDomainName(user.socialLinks.linkedin)}
-              </a>
-              <LocationOnIcon
-                fontSize="small"
+                <IconButton size="small" sx={{ color: "text.secondary" }}>
+                  <LinkedIn fontSize="small" />
+                </IconButton>
+                <Typography
+                  variant="body2"
+                  component="span"
+                  sx={{ color: "text.secondary" }}
+                >
+                  LinkedIn
+                </Typography>
+              </Link>
+
+              <Typography
+                variant="body2"
+                component="span"
                 sx={{ color: "text.secondary" }}
-              />
-              {user.profile.city}
-            </Typography>
+              >
+                <IconButton size="small" sx={{ color: "text.secondary" }}>
+                  <LocationOnIcon fontSize="small" />
+                </IconButton>
+                {user?.profile?.city}, {user?.profile?.country}
+              </Typography>
+            </Box>
           </Box>
-          <Divider sx={{ my: 1 }} />
-          <Typography variant="body1" textAlign="center">
-            <i>{user.profile.headline}</i>
+          <Divider />
+          <Typography
+            variant="body1"
+            textAlign="center"
+            component="p"
+            sx={{ my: 1 }}
+          >
+            <i>{user?.profile?.headline}</i>
           </Typography>
-          <Box container spacing={2}>
+          <Grid container gap={1}>
             <Grid item xs={12}>
               <Typography
                 variant="h6"
                 sx={{ fontWeight: 700, color: "#004275" }}
+                component="h2"
               >
                 Experience
               </Typography>
@@ -107,16 +146,14 @@ const TemplateCard3 = ({ user }) => {
                 <Box key={index}>
                   <Box display="flex" justifyContent="space-between" mt={1}>
                     <Typography variant="body2" sx={{ fontWeight: 700 }}>
-                      {exp.title} at {exp.company}
+                      {exp?.title} at {exp?.company}
                     </Typography>
                     <Typography variant="body2">
-                      {formatDate(exp.startDate)} -{" "}
-                      {exp.isCurrent ? "Present" : formatDate(exp.endDate)}
+                      {formatDate(exp?.startDate)} -{" "}
+                      {exp?.isCurrent ? "Present" : formatDate(exp?.endDate)}
                     </Typography>
                   </Box>
-                  <Typography variant="body2" mb={1}>
-                    {exp.description}
-                  </Typography>
+                  <Typography variant="body2">{exp?.description}</Typography>
                 </Box>
               ))}
             </Grid>
@@ -124,27 +161,27 @@ const TemplateCard3 = ({ user }) => {
               <Typography
                 variant="h6"
                 sx={{ fontWeight: 700, color: "#004275" }}
+                component="h2"
               >
                 Education
               </Typography>
               <Divider />
-
               {user.education.map((edu, index) => (
                 <Box key={index}>
                   <Box display="flex" justifyContent="space-between" mt={1}>
                     <Typography variant="body2" sx={{ fontWeight: 600 }}>
-                      {edu.institution}
+                      {edu?.institution}
                     </Typography>
                     <Typography variant="body2">
-                      {formatDate(edu.startDate)} - {formatDate(edu.endDate)}
+                      {formatDate(edu?.startDate)} - {formatDate(edu?.endDate)}
                     </Typography>
                   </Box>
                   <Typography variant="body2" sx={{ color: "text.secondary" }}>
-                    {edu.degree} in {edu.fieldOfStudy} {"("}Grade: {edu.grade} |
-                    CGPA: {edu.cgpa}
+                    {edu?.degree} in {edu?.fieldOfStudy} {"("}Grade:{" "}
+                    {edu?.grade} | CGPA: {edu?.cgpa}
                     {")"}
                   </Typography>
-                  <Typography variant="body2">{edu.activities}</Typography>
+                  <Typography variant="body2">{edu?.activities}</Typography>
                 </Box>
               ))}
             </Grid>
@@ -153,28 +190,28 @@ const TemplateCard3 = ({ user }) => {
               <Typography
                 variant="h6"
                 sx={{ fontWeight: 700, color: "#004275" }}
-                mt={1}
+                component="h2"
               >
                 Training & Courses
               </Typography>
               <Divider />
               {user.training.map((course, index) => (
-                <Box key={index} mb={1}>
+                <Box key={index}>
                   <Box display="flex" justifyContent="space-between" mt={1}>
                     <Typography variant="body2" sx={{ fontWeight: 600 }}>
-                      {course.courseName}
+                      {course?.courseName}
                     </Typography>
                     <Typography variant="body2">
-                      {formatDate(course.startDate)} -{" "}
-                      {formatDate(course.endDate)}
+                      {formatDate(course?.startDate)} -{" "}
+                      {formatDate(course?.endDate)}
                     </Typography>
                   </Box>
                   <Typography variant="body2">
-                    {course.organization} {"("}
-                    {course.location}
+                    {course?.organization} {"("}
+                    {course?.location}
                     {")"}
                   </Typography>
-                  <Typography variant="body2">{course.description}</Typography>
+                  <Typography variant="body2">{course?.description}</Typography>
                 </Box>
               ))}
             </Grid>
@@ -183,6 +220,7 @@ const TemplateCard3 = ({ user }) => {
               <Typography
                 variant="h6"
                 sx={{ fontWeight: 700, color: "#004275" }}
+                component="h2"
               >
                 Projects
               </Typography>
@@ -192,17 +230,20 @@ const TemplateCard3 = ({ user }) => {
                   <Box display="flex" justifyContent="space-between" mt={1}>
                     <Typography variant="body2" sx={{ fontWeight: 600 }}>
                       <Link
-                        href={proj.projectLink}
+                        to={proj?.projectLink}
+                        target="_blank"
+                        rel="noopener noreferrer"
                         style={{ textDecoration: "none", color: "inherit" }}
                       >
-                        {proj.projectName}
+                        {proj?.projectName}
                       </Link>
                     </Typography>
                     <Typography variant="body2">
-                      {formatDate(proj.startDate)} - {formatDate(proj.endDate)}
+                      {formatDate(proj?.startDate)} -{" "}
+                      {formatDate(proj?.endDate)}
                     </Typography>
                   </Box>
-                  <Typography variant="body2">{proj.description}</Typography>
+                  <Typography variant="body2">{proj?.description}</Typography>
                 </Box>
               ))}
             </Grid>
@@ -212,24 +253,24 @@ const TemplateCard3 = ({ user }) => {
               <Typography
                 variant="h6"
                 sx={{ fontWeight: 700, color: "#004275" }}
-                mt={1}
+                component="h2"
               >
                 Skills
               </Typography>
               <Divider />
               <Box sx={{ display: "flex", mt: 1 }}>
                 <Typography variant="body2">
-                  {user.skills.map((skill) => skill.skill).join(", ")}
+                  {user.skills.map((skill) => skill?.skill).join(", ")}
                 </Typography>
               </Box>
             </Grid>
-          </Box>
+          </Grid>
         </Box>
         <Button
           variant="contained"
           color="primary"
           onClick={() =>
-            handleView(user.profile.firstName + user.profile.lastName)
+            handleView(user?.profile?.firstName + user?.profile?.lastName)
           }
           sx={{ marginTop: "20px" }}
         >
