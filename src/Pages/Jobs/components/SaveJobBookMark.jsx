5d526d80387229d8b79208d2fa336070b39bfd66
anusha <anu636238@gmail.com>
2024-08-22 11:15:30 +0530

    reshare link copied

--- src/Pages/Jobs/components/SaveJobBookMark.jsx
+++ src/Pages/Jobs/components/SaveJobBookMark.jsx
@@ -1,33 +1,30 @@
-import { IconButton } from '@mui/material'
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { toggleSavedJob } from '../../../features/jobs/jobActions';
-import BookmarkIcon from '@mui/icons-material/Bookmark';
-import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import { IconButton } from "@mui/material";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleSavedJob } from "../../../features/jobs/jobActions";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
 
 const SaveJobBookMark = ({ jobId }) => {
-    const dispatch = useDispatch();
-    const isSaved = useSelector((state) =>
-        state.jobs.jobs.find((j) => j._id === jobId)?.saved
-    );
-    const toggleSaveStatus = async (e) => {
-        e.stopPropagation();
-        dispatch(toggleSavedJob(jobId));
-    };
-    return (
-        <IconButton
-            onClick={toggleSaveStatus}
-            sx={{
-                position: 'absolute',
-                top: 10,
-                right: 10,
-                color: isSaved ? 'blue' : 'grey',
-            }}
-            aria-label="Save Job"
-        >
-            {isSaved ? <BookmarkIcon /> : <BookmarkBorderIcon />}
-        </IconButton>
-    )
-}
+  const dispatch = useDispatch();
+  const isSaved = useSelector(
+    (state) => state.jobs.jobs.find((j) => j._id === jobId)?.saved
+  );
+  const toggleSaveStatus = async (e) => {
+    e.stopPropagation();
+    dispatch(toggleSavedJob(jobId));
+  };
+  return (
+    <IconButton
+      onClick={toggleSaveStatus}
+      sx={{
+        color: isSaved ? "blue" : "grey",
+      }}
+      aria-label="Save Job"
+    >
+      {isSaved ? <BookmarkIcon /> : <BookmarkBorderIcon />}
+    </IconButton>
+  );
+};
 
-export default SaveJobBookMark
+export default SaveJobBookMark;
