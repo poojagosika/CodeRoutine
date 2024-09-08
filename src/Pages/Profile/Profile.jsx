99e1fed9a77aefb0bee549bd6be88643bfcc4081
Pooja Gosika <pooja.gosika@gmail.com>
2024-09-08 12:54:22 +0530

    profileActions-slice-creation and fetch userProfile

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,64 +1,85 @@
-import * as React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useParams, useNavigate } from "react-router-dom";
 import { Box, Container, Paper, CircularProgress, Button } from "@mui/material";
-import { getUserByUserName } from "../../Api/userApi";
-import { useNavigate, useParams } from "react-router-dom";
+import { fetchUserProfile } from "../../features/profile/profileActions";
+import {
+  selectUserProfile,
+  selectProfileLoading,
+  selectProfileError,
+} from "../../features/profile/profileSlice";
 import ProblemsSolved from "./ProblemsSolved";
 import Skills from "./Skills";
 import SocialLinks from "./SocialLinks/SocialLinks";
 import PersonalInformation from "./PersonalInformation/PersonalInformation";
-import CopyRight from "../../Component/CopyRight/CopyRight";
 import Training from "./Training/Training";
 import Project from "./Project/Project";
 import Education from "./Education/Education";
 import Experience from "./Experience/Experience";
-import { toast } from "react-toastify";
 import { ContextStore } from "../../Context/ContextStore";
 
 const Profile = () => {
-  const [userProfile, setUserProfile] = React.useState(null);
-  const [loading, setLoading] = React.useState(true);
+  const dispatch = useDispatch();
   const { id: userName } = useParams();
-  const { userData } = ContextStore();
   const navigate = useNavigate();
+  const userProfile = useSelector(selectUserProfile);
+  const loading = useSelector(selectProfileLoading);
+  const error = useSelector(selectProfileError);
+  const { userData } = ContextStore();
 
-  React.useEffect(() => {
+  useEffect(() => {
     document.title = "CodeRoutine | Profile";
-  }, []);
-
-  React.useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await getUserByUserName(userName);
-        setUserProfile(response.data.user);
-      } catch (error) {
-        toast.error(error?.response?.data?.message);
-        console.error("Error fetching data:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, [userName]);
+    dispatch(fetchUserProfile(userName));
+  }, [dispatch, userName]);
 
   const hasSocialLinks = (socialLinks) => {
     if (!socialLinks) return false;
     const { linkedin, github, x, blog, portfolio, additional } = socialLinks;
     return (
-      linkedin || github || x || blog || portfolio || (additional && additional.length > 0)
+      linkedin ||
+      github ||
+      x ||
+      blog ||
+      portfolio ||
+      (additional && additional.length > 0)
     );
   };
 
   const hasPersonalInformation = (profile) => {
     if (!profile) return false;
-    const { firstName, lastName, headline, currentPosition, education, city, gender, country } = profile;
+    const {
+      firstName,
+      lastName,
+      headline,
+      currentPosition,
+      education,
+      city,
+      gender,
+      country,
+    } = profile;
     return (
-      firstName || lastName || headline || currentPosition || education || city || gender || country
+      firstName ||
+      lastName ||
+      headline ||
+      currentPosition ||
+      education ||
+      city ||
+      gender ||
+      country
     );
   };
 
   const isLogin = () => {
-    return userData?._id && userProfile?._id && userData._id === userProfile._id;
+    return (
+      userData?._id && userProfile?._id && userData._id === userProfile._id
+    );
+  };
+
+  const renderSection = (Component, condition) => {
+    if (isLogin() || condition) {
+      return <Component userProfile={userProfile} />;
+    }
+    return null;
   };
 
   if (loading) {
@@ -76,18 +97,22 @@ const Profile = () => {
     );
   }
 
-  const renderSection = (Component, condition) => {
-    if (isLogin() || condition) {
-      return <Component userProfile={userProfile} />;
-    }
-    return null;
-  };
+  if (error) {
+    return (
+      <Container
+        maxWidth="lg"
+        style={{ marginTop: "80px", marginBottom: "100px" }}
+      >
+        <div>Error: {error}</div>
+      </Container>
+    );
+  }
 
   return (
     <>
       <Container
         maxWidth="lg"
-        style={{ marginTop: "40px", marginBottom: "80px", minHeight:"100vh" }}
+        style={{ marginTop: "40px", marginBottom: "80px", minHeight: "100vh" }}
       >
         <Paper elevation={3} sx={{ borderRadius: "10px", overflow: "hidden" }}>
           <Box
@@ -98,11 +123,11 @@ const Profile = () => {
               textAlign: "center",
               height: "200px",
               display: "flex",
-              justifyContent: 'flex-end',
+              justifyContent: "flex-end",
               alignItems: "flex-end",
             }}
           >
-            {isLogin() &&
+            {isLogin() && (
               <Box
                 display="flex"
                 gap={2}
@@ -122,29 +147,23 @@ const Profile = () => {
                 >
                   Resume
                 </Button>
-                {/* <Button
-                  variant="contained"
-                  color="primary"
-                  style={{ marginTop: "20px" }}
-                  onClick={() =>
-                    navigate(`/resume/${userProfile?.userName}`, {
-                      state: { userProfile },
-                    })
-                  }
-                >
-                  Custom Resume
-                </Button> */}
               </Box>
-            }
+            )}
           </Box>
           <Container>
-            {renderSection(PersonalInformation, hasPersonalInformation(userProfile?.profile))}
+            {renderSection(
+              PersonalInformation,
+              hasPersonalInformation(userProfile?.profile)
+            )}
             {renderSection(Experience, userProfile?.experience?.length > 0)}
             {renderSection(Education, userProfile?.education?.length > 0)}
             {renderSection(Training, userProfile?.training?.length > 0)}
             {renderSection(Project, userProfile?.project?.length > 0)}
             {renderSection(Skills, userProfile?.skills?.length > 0)}
-            {renderSection(SocialLinks, hasSocialLinks(userProfile?.socialLinks))}
+            {renderSection(
+              SocialLinks,
+              hasSocialLinks(userProfile?.socialLinks)
+            )}
             <ProblemsSolved problemsSolved={userProfile?.problemsSolved} />
           </Container>
         </Paper>
