26fc31b7b648f98dcfc66b4872cbaa04bebc3c62
anusha <anu636238@gmail.com>
2024-10-07 11:53:27 +0530

    hiringtalents

---
+++ client/src/Pages/Jobs/HireTalent/Hiretalent.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Box } from "@mui/material";
+import Sidebar from "./component/Sidebar";
+import Dashboard from "./pages/Dashboard";
+import VirtualHiring from "./pages/VirtualHiring";
+import CampusHiring from "./pages/CampusHiring";
+import { Route, Routes } from "react-router-dom";
+
+function Hiretalent() {
+  return (
+    <Box sx={{ display: "flex", height: "100vh" }}>
+      {/* Sidebar */}
+      <Sidebar />
+
+      {/* Main Content Area */}
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          overflow: "scroll",
+          p: 3,
+        }}
+      >
+        <Routes>
+          {/* Base Dashboard Route */}
+          <Route path="/" element={<Dashboard />} />
+          <Route path="virtual-hiring" element={<VirtualHiring />} />
+          <Route path="campus-hiring" element={<CampusHiring />} />
+        </Routes>
+      </Box>
+    </Box>
+  );
+}
+
+export default Hiretalent;
---
+++ client/src/Pages/Jobs/HireTalent/pages/VirtualHiring.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function VirtualHiring() {
+  return <div>VirtualHiring</div>;
+}
+
+export default VirtualHiring;
